7a5167948393557f2885e24ae09dafe35b55318c
Ryan Jentzsch <ryan.jentzsch@gmail.com>
2019-01-06 01:35:19 -0700

    TSLinter code fixes
    - AdminPage.tsx
    - HouseholdPage.tsx
    - IntakeEdit.tsx
    - IntakeGrid.tsx
    - IntakePage.tsx
    - LoginPage.tsx
    - MemberBadge.tsx
    - MemberGrid.tsx
    - MemberPanel.tsx
    - MemberEdit.tsx
    - PovertyEdit.tsx
    - PovertyGrid.tsx
    - PovertyPage.tsx
    - PovertyPanel.tsx
    - SearchPage.tsx
    - UserEdit.tsx
    - UserGrid.tsx
    - UserPage.tsx
    - CountyDropdown.tsx
    - ErrorAlert.tsx
    - ImageDrop.tsx
    - LandingPage.tsx
    - StateDropdown.tsx
    - StoreContext.tsx
    - models
    - providers
    - typings
    - utils

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -78,6 +78,7 @@ export function isNickNameValid(str: string): boolean
 export function isEmailValid(emailStr: string): boolean
 {
     if (emailStr.length >= 3) {
+        // tslint:disable-next-line: max-line-length
         return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailStr);
     }
     return false;
