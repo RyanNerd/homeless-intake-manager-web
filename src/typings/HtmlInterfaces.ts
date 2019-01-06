1b616a061eea84b3557eca321d3b02da06a7017b
Ryan Jentzsch <ryan.jentzsch@gmail.com>
2019-01-06 01:27:13 -0700

    TSLinter code fixes
    - AdminPage.tsx
    - HouseholdPage.tsx
    - IntakeEdit.tsx
    - IntakeGrid.tsx
    - IntakePage.tsx
    - LoginPage.tsx
    - MemberBadge.tsx
    - MemberGrid.tsx
    - MemberPanel.tsx
    - MemberEdit.tsx
    - PovertyEdit.tsx
    - PovertyGrid.tsx
    - PovertyPage.tsx
    - PovertyPanel.tsx
    - SearchPage.tsx
    - UserEdit.tsx
    - UserGrid.tsx
    - UserPage.tsx
    - CountyDropdown.tsx
    - ErrorAlert.tsx
    - ImageDrop.tsx
    - LandingPage.tsx
    - StateDropdown.tsx
    - StoreContext.tsx
    - models
    - providers
    - typings

--- src/typings/HtmlInterfaces.ts
+++ src/typings/HtmlInterfaces.ts
@@ -1,28 +1,29 @@
 export interface IHTMLElement extends HTMLElement
 {
-    value: any
-    name: string
-    type: string
-    checked?: boolean
+    value: any;
+    name: string;
+    type: string;
+    checked?: boolean;
 }
 
 export interface ITarget extends EventTarget
 {
-    type: string
-    value: any
-    checked: boolean | null | undefined
-    name: string
+    type: string;
+    value: any;
+    checked: boolean | null | undefined;
+    name: string;
 }
 
 export interface INodeListOf extends NodeListOf<Element> {
-    [Symbol.iterator](): IterableIterator<IHTMLElement>
+    [Symbol.iterator](): IterableIterator<IHTMLElement>;
 }
 
-interface FormatValue {
-    formatValue: Function
+/* tslint:disable: ban-types */
+interface IFormatValue {
+    formatValue: Function;
 }
 
 export interface IDocument extends Document
 {
-    l10n: FormatValue
+    l10n: IFormatValue;
 }
--- src/typings/npm.d.ts
+++ src/typings/npm.d.ts
@@ -2,5 +2,4 @@
 // @see https://github.com/Microsoft/TypeScript/issues/2709
 declare module "*";
 
-
 declare function parseInt(s: string | number, radix?: number): number;
